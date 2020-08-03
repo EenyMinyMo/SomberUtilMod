8199b57c2eea747f4f78289a200fad3cc1b42313
SomberLaptop <SomberSoul43@gmail.com>
2020-08-03 21:30:34 +0500

    Переименование констант, добавление документации.

--- src/main/java/ru/somber/commonutil/Axis.java
+++ src/main/java/ru/somber/commonutil/Axis.java
@@ -1,7 +1,20 @@
 package ru.somber.commonutil;
 
+/**
+ * Перечисление для представления различных состояний,
+ * связанных с осями координат.
+ */
 public enum Axis {
 
-    AXIS_X, AXIS_Y, AXIS_Z, NONE_AXIS;
+    /** Ось абсцисс. (ось OX) */
+    ABSCISSA_AXIS,
+    /** Ось ординат. (ось OY) */
+    ORDINATE_AXIS,
+    /** Ось аппликат. (ось OZ) */
+    APPLICATE_AXIS,
+    /** Все оси сразу. */
+    ALL_AXIS,
+    /** Не для какой из осей. (т.е. от оси не зависит) */
+    NONE_AXIS;
 
 }
