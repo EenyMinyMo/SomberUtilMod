65da3dd5eb579d1740feaa2ffb1559a6ef0e270a
SomberLaptop <SomberSoul43@gmail.com>
2020-08-03 13:20:55 +0500

    Забыл удалить лишнюю строчку

--- src/main/java/ru/somber/SomberUtilMod.java
+++ src/main/java/ru/somber/SomberUtilMod.java
@@ -24,7 +24,7 @@ public class SomberUtilMod {
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-        System.out.println("post init " + event.description());
+
     }
 
 }
